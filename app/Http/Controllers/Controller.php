dfe995b68b1e798b870c72860092c6d2b8ae7a30
tee1bn <tee01bn@gmail.com>
2018-02-09 06:26:00 -0800

    resolve controller conflict

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,68 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+use App\User;
+use App\EWallet;
+use App\Notification;
+
+
+
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+
+
+
+ /**
+     * Subscribe user to a Subscription either upgrade or downgrade.
+     * @param user_id, subscription_id 
+     * 
+     */
+
+public function subscribe($user_id, $new_subscription_id)
+{    
+     User::where('id', $user_id)->update([
+               'on_subscription_id' => $new_subscription_id
+     ]);       
+}
+
+
+
+ /**
+     * Fetch user profile attributes from all asscociated models.
+     * @param user_id  
+     * @return array   
+     * 
+     */
+
+public function create_notification($user_id, $notification_body, $notification_title, $url)
+{
+
+
+
+Notification::create([
+                    'user_id'=> $user_id,
+                    'notification_body'=> $notification_body,
+                    'notification_title'=> $notification_title,
+                    'url' => $url
+                    ]);
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
 }
